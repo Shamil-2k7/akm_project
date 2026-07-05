853ca0245e22d2a4add5bb3ce1b5266b5faff638
shamil_k <shamil2k7g@gmail.com>
2026-07-05 10:16:11 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL ='https://akm-project.onrender.com';
+const API_URL =const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://akm-project.onrender.com/api'
 
 // Get or create a persistent unique Device ID for the client
 export const getDeviceId = () => {
